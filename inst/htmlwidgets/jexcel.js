c5834d516b2e65f1fc3f256d665309ad369fc1be
Hans Liang <hansdesheng@gmail.com>
2020-02-27 11:45:57 +0100

    Delete redundant code? + Fix Style Guides

--- inst/htmlwidgets/jexcel.js
+++ inst/htmlwidgets/jexcel.js
@@ -16,22 +16,36 @@
           var otherParams = {};
 
           Object.keys(params).forEach(function(ky) {
-            if(ky !== "dateFormat" && ky !== "rowHeight" && ky !== "otherParams" ) {
+
+            if(
+               ky !== "dateFormat" &&
+               ky !== "rowHeight" &&
+               ky !== "otherParams"
+               ) {
+
               // Check if the key is columns and check if the type is calendar, if yes add the date format
               if(ky === "columns" && dateFormat !== "DD/MM/YYYY"){
+
                 otherParams[ky] = params[ky].map(function(column){
+
                   if(column.type === "calendar"){
+
                     column.options = {format: dateFormat}
+
                   }
+
                   return column
+
                 })
-              
+
                 return;
+              }
             }
-          }
+
             otherParams[ky] = params[ky];
+
           });
-    
+
           var rows = (function() {
             if (rowHeight) {
               const rows = {};
@@ -71,24 +85,35 @@
           ]
           }
 
-          // If new instance of the table   
+          // If new instance of the table
           if(excel === null) {
+
             excel =  jexcel(container, otherParams);
             container.excel = excel;
-    
+
             return;
+
           }
 
-          var  selection  = excel.selectedCell;
+          var selection = excel.selectedCell;
 
           while (container.firstChild) {
+
             container.removeChild(container.firstChild);
-        }
+
+          }
 
           excel = jexcel(container, otherParams);
-          
+
           if(selection){
-            excel.updateSelectionFromCoords(selection[0], selection[1], selection[2], selection[3]);
+
+            excel.updateSelectionFromCoords(
+                                            selection[0],
+                                            selection[1],
+                                            selection[2],
+                                            selection[3]
+                                            );
+
           }
 
           container.excel = excel;
@@ -102,53 +127,53 @@
         onChange: function(obj){
 
           if (HTMLWidgets.shinyMode) {
-        
+
             var colType = this.columns.map(function(column){
+
               return column.type;
+
             })
 
             var colHeaders = this.colHeaders;
 
-            if(this.colHeaders.every(function (val){return (val ==='')})){
-              var colHeaders = this.columns.map(function(column){ return column.title})
-            }
+            Shiny.setInputValue(obj.id, {
 
-            Shiny.setInputValue(obj.id, 
-              {
-                data:this.data, 
+                data:this.data,
                 colHeaders: colHeaders,
                 colType: colType,
+
               })
           }
         },
 
         onChangeHeader: function(obj, column, oldValue, newValue){
-    
+
           if (HTMLWidgets.shinyMode) {
 
             var colHeaders = this.colHeaders;
-
-            if(this.colHeaders.every(function (val){return (val ==='')})){
-              var colHeaders = this.columns.map(function(column){ return column.title})
-            }
 
             var newColHeader = colHeaders;
             newColHeader[parseInt(column)] = newValue;
 
             var colType = this.columns.map(function(column){
+
               return column.type;
+
             })
 
-            Shiny.setInputValue(obj.id, 
-              {
-                data:this.data, 
+            Shiny.setInputValue(obj.id, {
+
+                data:this.data,
                 colHeaders: newColHeader,
                 colType: colType
+
               })
           }
         }
+
       };
     }
+
   });
 
 
@@ -156,7 +181,7 @@
 
     // This function is used to set comments in the table
     Shiny.addCustomMessageHandler("excelR:setComments", function(message) {
-   
+
       var el = document.getElementById(message[0]);
       if (el) {
         el.excel.setComments(message[1], message[2]);
@@ -168,12 +193,16 @@
 
       var el = document.getElementById(message[0]);
       if (el) {
+
         var comments = message[1] ? el.excel.getComments(message[1]): el.excel.getComments(null);
 
-        Shiny.setInputValue(message[0], 
-          {
+        Shiny.setInputValue(message[0], {
+
            comments
-          });
+
+        });
+
       }
+
     });
   }
